526e305c92ebb259a535f5629eeb92fd48de6845
Samuel Urah Yahaya <urahsamuel0202@gmail.com>
2025-07-30 03:50:00 +0100

    feat: add authentication layout and pages with sign in and sign up functionality
    - Added AuthLayout component for authentication pages.
    - Created SignInPage and SignUpPage components.
    - Implemented SignInCard and SignUpCard components for user login and registration.
    - Introduced DottedSeparator component for visual separation in forms.
    - Updated layout to use Google Fonts and removed local font imports.
    - Added logo SVG to public assets.
    - Included react-icons for social login buttons.
    - Updated package.json to include react-icons dependency.

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,67 @@
+import DottedSeparator from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+const SignInCard = () => {
+  return (
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">Welcome Back!</CardTitle>
+      </CardHeader>
+      <div className="px-7 mb-2">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <form className="space-y-4">
+          <Input
+            required
+            type="email"
+            placeholder="Enter your email address"
+            onChange={() => {}}
+          />
+          <Input
+            required
+            type="password"
+            placeholder="Enter your password"
+            onChange={() => {}}
+            min={8}
+            max={256}
+          />
+          <Button disabled={false} size={"lg"} className="w-full">
+            Login
+          </Button>
+        </form>
+      </CardContent>
+
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button
+          variant="secondary"
+          size={"lg"}
+          className="w-full"
+          disabled={false}
+        >
+          <FcGoogle className="mr-2 size-5" />
+          Login with Google
+        </Button>
+        <Button
+          variant="secondary"
+          size={"lg"}
+          className="w-full"
+          disabled={false}
+        >
+          <FaGithub className="mr-2 size-5" />
+          Login with Github
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+export default SignInCard;
