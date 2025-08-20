5534f7f2d741e5bb0afe2b0585713d458026b1b0
Samuel Urah Yahaya <urahsamuel0202@gmail.com>
2025-08-21 00:57:56 +0100

    refactor: migrate auth and workspace actions to queries for improved structure

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,25 +1,14 @@
-import { cookies } from "next/headers";
-import { Databases, Client, Query, Account } from "node-appwrite";
-import { AUTH_COOKIE } from "@/features/auth/constants";
+import { Query } from "node-appwrite";
+
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
 import { getMember } from "../members/utils";
 import { Workspace } from "./types";
+import { createSessionClient } from "@/lib/appwrite";
 
 export const getWorkspaces = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+    const { databases, account } = await createSessionClient();
 
-    const session = cookies().get(AUTH_COOKIE);
-
-    if (!session) {
-      return { documents: [], total: 0 };
-    }
-
-    client.setSession(session.value);
-    const databases = new Databases(client);
-    const account = new Account(client);
     const user = await account.get();
 
     const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
@@ -40,7 +29,7 @@ export const getWorkspaces = async () => {
 
     return workspaces;
   } catch {
-    return null;
+    return { documents: [], total: 0 };
   }
 };
 
@@ -50,19 +39,8 @@ interface GetWorkspaceProps {
 
 export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+    const { databases, account } = await createSessionClient();
 
-    const session = cookies().get(AUTH_COOKIE);
-
-    if (!session) {
-      return null;
-    }
-
-    client.setSession(session.value);
-    const databases = new Databases(client);
-    const account = new Account(client);
     const user = await account.get();
 
     const member = await getMember({
